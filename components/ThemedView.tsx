858e616130bf811dd065814ffda19cb5538caf28
tgkd <p.y.trofimov@gmail.com>
2026-04-05 09:25:41 +0200

    deps

--- components/ThemedView.tsx
+++ components/ThemedView.tsx
@@ -1,7 +1,6 @@
 import { View, type ViewProps } from "react-native";
 
 import { useThemeColor } from "@/hooks/useThemeColor";
-import { Colors } from "@/constants/Colors";
 
 export type ThemedViewProps = ViewProps & {
   lightColor?: string;
